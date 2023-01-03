644040fc29ae852c20698e94d5a82a32487519f2
Rohit <Rohit@spotblhol0128.awsspoton.com>
2023-01-03 17:52:48 +0530

    Added: UC1 Binary search tree.

--- src/com/bridgelabz/BinarySearchTree.java
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -1,7 +1,44 @@
 package com.bridgelabz;
 
+package com.bridgelabz.BST;
+
+//UC-1_Create_BST
+
 public class BinarySearchTree {
+    private TreeNode root;
+
+    private class TreeNode{
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+
+    public void insert(int value){
+        root = insert(root, value);
+    }
+
+    public TreeNode insert( TreeNode root, int value){
+        if (root == null || root.data == value){
+            root = new TreeNode(value);
+            return root;
+        }
+        if (value < root.data){
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to binary search tree.");
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(56);
+        tree.insert(30);
+        tree.insert(70);
+
     }
 }
